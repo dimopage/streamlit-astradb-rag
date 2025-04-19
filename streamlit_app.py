33d8a20499e7c1548dd4d65553cd7df72ada493d
D!Mo <hi@dimo.page>
2025-04-19 15:09:41 +0000

    renove style

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.markdown("""
 
 
 # Header
-st.markdown('<div">GenIAlab.Space</div>', unsafe_allow_html=True)
+st.markdown('<div>GenIAlab.Space</div>', unsafe_allow_html=True)
 st.title("DocVectorizer for RAG")
 
 # Upload section
